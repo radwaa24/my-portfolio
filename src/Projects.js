4df35df180b49aee7dd345e1af1ff0ffbef5c035
radwa <radwa2484@gmail.com>
2023-04-18 04:46:10 +0200

    second update

--- src/Projects.js
+++ src/Projects.js
@@ -30,29 +30,29 @@ const Projects = () => {
 
       <article className='grid lg:ml-20 w-full p-4 ml-10
       bg-red-100 hover:bg-red-50 text-center rounded-3xl'>
-
+         <a href='https://little-limon.vercel.app/' target='_blank' rel="noreferrer" >
         <img src={littlelimon} alt='little-limon'
         className='' />
         <h1 className='font-bold font-serif text-slate-800 hover:font-mono'>Little Limon Resturant</h1> 
-        <p className='font-serif text-slate-800'> a capstone project guided by Meta Front end developer , using React.JS and other UI frame works for a simple Resturant's website including reservation page .</p>
+        <p className='font-serif text-slate-800'> a capstone project guided by Meta Front end developer , using React.JS and other UI frame works for a simple Resturant's website including reservation page .</p> </a>
       </article>
 
     <article className='grid col-start-3 justify-self-end lg:mr-20 w-full mr-10
     bg-red-100 p-4 hover:bg-red-50 text-center rounded-3xl'>
-
+        <a href='https://todobyradwa-j8qudojxy-radwaa24.vercel.app/'target='_blank' rel="noreferrer" >
         <img src={todo} alt='todo'
         className='w-full ' />
         <h1 className='font-bold font-serif text-slate-800 hover:font-mono'> TODO List APP </h1> 
-        <p className='font-serif text-slate-800'> A simple App to manage daily tasks by adding items to do and complete or remove them and also disply both using only CSS for styling .</p>
+        <p className='font-serif text-slate-800'> A simple App to manage daily tasks by adding items to do and complete or remove them and also disply both using only CSS for styling .</p> </a>  
       </article>
 
       <article className=' mx-20 col-span-3 justify-self-center items-center lg:mx-96 mt-8
     bg-red-100 p-5 hover:bg-red-50 text-center rounded-3xl'>
-
+        <a href='https://calcbyradwa.vercel.app/' target='_blank' rel="noreferrer">
         <img src={calc} alt='calculator'
         className='w-full ' />
         <h1 className='font-bold font-serif text-slate-800 hover:font-mono py-5'> Basic calculator </h1>
-        <p className='font-serif text-slate-800'> Using React hooks and events .</p>
+        <p className='font-serif text-slate-800'> Using React hooks and events .</p> </a>
       </article>
       </div>
     </div>
